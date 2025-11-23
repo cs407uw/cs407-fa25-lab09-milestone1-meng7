4cb8a9d9530e38dd84235c92f159d3428af46ab5
soumoe <mcao62@wisc.edu>
2025-11-23 13:35:39 -0600

    milestone 1

--- app/src/main/java/com/cs407/lab09/BallViewModel.kt
+++ app/src/main/java/com/cs407/lab09/BallViewModel.kt
@@ -17,6 +17,11 @@ class BallViewModel : ViewModel() {
     private val _ballPosition = MutableStateFlow(Offset.Zero)
     val ballPosition: StateFlow<Offset> = _ballPosition.asStateFlow()
 
+    // !!! 新增：像素/米 的比例系数 !!!
+    // 如果觉得还是慢，就把这个数改大 (例如 500, 800)
+    // 如果觉得太快控制不住，就把这个数改小 (例如 200)
+    private val SIMULATION_RATE = 400f
+
     fun initBall(fieldWidth: Float, fieldHeight: Float, ballSizePx: Float) {
         if (ball == null) {
             ball = Ball(fieldWidth, fieldHeight, ballSizePx)
@@ -33,8 +38,8 @@ class BallViewModel : ViewModel() {
             if (lastTimestamp != 0L) {
                 val dT = (event.timestamp - lastTimestamp) / 1_000_000_000.0f
 
-                val xAcc = -event.values[0]
-                val yAcc = event.values[1]
+                val xAcc = -event.values[0] * SIMULATION_RATE
+                val yAcc = event.values[1] * SIMULATION_RATE
 
                 currentBall.updatePositionAndVelocity(xAcc, yAcc, dT)
 
